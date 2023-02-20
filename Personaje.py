d0b6c3b067cb5bc7dc165f186cef7158d5fdd1b5
ARP2112 <121040859@upq.edu.mx>
2023-02-20 15:29:54 -0600

    Se creo el metodo correr

---
+++ Personaje.py
@@ -0,0 +1,14 @@
+class Personaje:
+    
+    #atributos
+    especie="Humano"
+    nombre="MasterChief"
+    altura=2.70
+    
+    #metodos
+    def correr(self,status):
+        if(status):
+            print("El personaje "+self.nombre +"esta corriendo")
+        else:
+            print("El personaje "+ self.nombre+"se detuvo")
+            
